a3ab4f90f38fb10b69b42d4b220cc667f5f801c2
idan41601 <idan41601@gmail.com>
2026-05-14 11:33:39 +0300

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient(
-  'https://bfszysdzxttrwbbeuqoh.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJmc3p5c2R6eHR0cndiYmV1cW9oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg3MTE2NzgsImV4cCI6MjA5NDI4NzY3OH0.F8fzijnZtHJH3DhebJKbdgM4_s4AaYsxLHcqV4UD8zM'
-)
+const URL = 'https://bfszysdzxttrwbbeuqoh.supabase.co'
+const KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJmc3p5c2R6eHR0cndiYmV1cW9oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg3MTE2NzgsImV4cCI6MjA5NDI4NzY3OH0.F8fzijnZtHJH3DhebJKbdgM4_s4AaYsxLHcqV4UD8zM'
+
+export const supabase = createClient(URL, KEY)
